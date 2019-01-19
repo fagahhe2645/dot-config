6c3c3c1c652f002b6d527c678e59eb1f6559fe1b
Shuya Morimoto <fagahhe2645@Reuben.local>
2019-01-19 18:20:27 +0900

    zplug plugin update

--- zsh/zplug.sh
+++ zsh/zplug.sh
@@ -9,6 +9,20 @@ zplug "zsh-users/zsh-syntax-highlighting", defer:2
 zplug "zsh-users/zsh-history-substring-search", defer:3
 zplug "mafredri/zsh-async", from:github
 zplug "sindresorhus/pure", use:pure.zsh, from:github, as:theme
+# https://qiita.com/mollifier/items/81b18c012d7841ab33c3
+zplug "mollifier/anyframe"
+zplug "fagahhe2645/5a0daab3bb10b947eccadb0c9ab02d5e", from:gist
+zplug "fagahhe2645/95359f71b1670cfb7c8673240b2225df", from:gist
+# commands
+zplug "junegunn/fzf-bin", as:command, from:gh-r, rename-to:fzf
+zplug "peco/peco", as:command, from:gh-r
+zplug "github/hub", as:command, from:gh-r
+zplug "motemen/ghq", as:command, from:gh-r
+zplug "b4b4r07/gist", as:command, from:gh-r
+
+# settings 
+alias github='cd $(ghq root)/$(ghq list | peco)'
+bindkey '^r' anyframe-widget-put-history
 
 
 # Install plugins if there are plugins that have not been installed
